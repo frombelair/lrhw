433f2710a497d22ab935d597a72af9cf7192142d
frombelair <lbenchouk@gmail.com>
2016-08-22 18:12:35 -0400

    changed washed car

--- ex1.rb
+++ ex1.rb
@@ -1,1 +1,8 @@
-test 2
+puts "hello world!"
+puts "hello again"
+puts "I like typing this"
+puts "This is fun" 
+puts "yay! Printing."
+puts "I`d much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "washed my car"
